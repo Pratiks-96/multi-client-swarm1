32bac101e1c6f6b126fb32ae7e12f51462a0f39b
Pratiks-96 <pratiksavant96@gmail.com>
2026-02-22 12:20:05 +0530

    Update main.py

--- client-b/main.py
+++ client-b/main.py
@@ -1,11 +1,27 @@
 from fastapi import FastAPI
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
+from fastapi.responses import Response
 
 app = FastAPI()
 
+# metric
+REQUEST_COUNT = Counter(
+    "client_b_requests_total",
+    "Total requests for Client B"
+)
+
 @app.get("/")
 async def root():
+    REQUEST_COUNT.inc()
     return {"message": "Client B Root!"}
 
 @app.get("/health")
 async def health():
     return {"status": "ok"}
+
+@app.get("/metrics")
+async def metrics():
+    return Response(
+        generate_latest(),
+        media_type=CONTENT_TYPE_LATEST
+    )
